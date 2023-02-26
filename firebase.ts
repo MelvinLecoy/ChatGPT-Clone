1397e1db37cbb68704ecf91a1da6ff89a0345728
Kwan Ting Lau <kestino@iMac.local>
2023-02-26 00:25:08 -0500

    Updated

--- firebase.ts
+++ firebase.ts
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCpUPkDI_0I2y_GiqCLMrATbQHO2MlEkpc",
-    authDomain: "travelourgpt.firebaseapp.com",
-    projectId: "travelourgpt",
-    storageBucket: "travelourgpt.appspot.com",
-    messagingSenderId: "318042893194",
-    appId: "1:318042893194:web:079f538260e1d86764b673"
-  };
+  apiKey: "AIzaSyCpUPkDI_0I2y_GiqCLMrATbQHO2MlEkpc",
+  authDomain: "travelourgpt.firebaseapp.com",
+  projectId: "travelourgpt",
+  storageBucket: "travelourgpt.appspot.com",
+  messagingSenderId: "318042893194",
+  appId: "1:318042893194:web:079f538260e1d86764b673"
+};
 
 // Initialize Firebase
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
